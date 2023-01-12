4ab02986d39ba9b301ede348df7287a2ea3e11b1
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-01-12 20:42:50 +0530

    header and tabs completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import { useState } from 'react';
-
+import Logo from './assets/react-logo.png';
+import Header from './components/Header';
+import Tabs from './components/Tabs';
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div>hello world</div>;
+  return (
+    <div className="main-container">
+      <Header />
+      <Tabs />
+    </div>
+  );
 }
 
 export default App;
