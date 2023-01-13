d5ad6c08a02d1af3aa794062c2683454a143e912
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-01-13 15:24:15 +0530

    form completed without validations

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Logo from './assets/react-logo.png';
 import Header from './components/Header';
 import Tabs from './components/Tabs';
+import Form from './components/Form';
 function App() {
   const [count, setCount] = useState(0);
 
@@ -9,6 +10,7 @@ function App() {
     <div className="main-container">
       <Header />
       <Tabs />
+      <Form />
     </div>
   );
 }
