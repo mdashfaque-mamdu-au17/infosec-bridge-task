1f7788696498cf07fd80fb925b6c5be21837006d
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-01-13 16:58:49 +0530

    single social link

---
+++ src/components/SocialLink.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SocialLink = (props) => {
+  return (
+    <a href="" className="social-link">
+      {props.children}
+    </a>
+  );
+};
+
+export default SocialLink;
